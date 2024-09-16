81948e139569f70f92b5a9a689ffdf3348135347
Alina <alina.mikhaylova.03@gmail.com>
2024-09-16 04:35:23 +0300

    диалоговое окно

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,19 @@
 import '../styles/components.scss';
+
+const callForm = document.getElementById('callForm');
+const openDialogBtn = document.getElementById('callFormBtn');
+
+openDialogBtn.addEventListener('click', (event) => {
+  event.stopPropagation();
+  callForm.classList.add('active');
+  document.querySelector('body').classList.add('no-scroll');
+});
+
+callForm.children[0].addEventListener('click', (e) => {
+  e.stopPropagation();
+});
+
+document.addEventListener('click', () => {
+  callForm.classList.remove('active');
+  document.querySelector('body').classList.remove('no-scroll');
+});
