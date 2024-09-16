71f9e6972dc7863df4e30755b36cbe33efca9e04
Alina <alina.mikhaylova.03@gmail.com>
2024-09-16 09:30:17 +0300

    больше рефакторинга, всплывашка успеха

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import '../styles/components.scss';
 
 const callForm = document.getElementById( 'callForm' );
 const openDialogBtn = document.getElementById( 'callFormBtn' );
+const form = document.querySelector( 'form' );
 
 // Открытие диалогового окна
 openDialogBtn.addEventListener( 'click', ( e ) => 
@@ -22,3 +23,27 @@ document.addEventListener( 'click', () =>
 	callForm.classList.remove( 'active' );
 	document.querySelector( 'body' ).classList.remove( 'no-scroll' );
 } );
+
+form.addEventListener( 'submit', (e) => 
+  {
+    e.preventDefault();
+
+    const fields = callForm.querySelector('.fields');
+    const lead = callForm.querySelector('p');
+    const btn = callForm.querySelector('.icon')
+
+    if (fields) 
+      {
+        fields.remove();
+    }
+
+    if (lead)
+      {
+        lead.textContent = "Спасибо! Наш менеджер свяжется с вами в ближайшее время и ответит на все интересующие вопросы."
+    }
+
+    if (btn)
+      {
+      btn.remove();
+    }
+});
