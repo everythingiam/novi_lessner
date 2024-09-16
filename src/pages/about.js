c80eb8a78d49b5220736d9b4511bf8d9e2ad1f31
Alina <alina.mikhaylova.03@gmail.com>
2024-09-16 08:52:39 +0300

    рефакторинг кода...

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,24 @@
 import '../styles/components.scss';
 
-const callForm = document.getElementById('callForm');
-const openDialogBtn = document.getElementById('callFormBtn');
+const callForm = document.getElementById( 'callForm' );
+const openDialogBtn = document.getElementById( 'callFormBtn' );
 
-openDialogBtn.addEventListener('click', (event) => {
-  event.stopPropagation();
-  callForm.classList.add('active');
-  document.querySelector('body').classList.add('no-scroll');
-});
+// Открытие диалогового окна
+openDialogBtn.addEventListener( 'click', ( e ) => 
+  {
+	e.stopPropagation();
+	callForm.classList.add( 'active' );
+	document.querySelector( 'body' ).classList.add( 'no-scroll' );
+} );
 
-callForm.children[0].addEventListener('click', (e) => {
-  e.stopPropagation();
-});
+callForm.children[0].addEventListener( 'click', ( e ) => 
+  {
+	e.stopPropagation();
+} );
 
-document.addEventListener('click', () => {
-  callForm.classList.remove('active');
-  document.querySelector('body').classList.remove('no-scroll');
-});
+// Закрытие диалогового окна
+document.addEventListener( 'click', () => 
+  {
+	callForm.classList.remove( 'active' );
+	document.querySelector( 'body' ).classList.remove( 'no-scroll' );
+} );
